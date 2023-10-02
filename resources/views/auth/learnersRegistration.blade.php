d7353e1c3e7b4667540e2e5f7706b9f14d8947a2
Zander Roi Tabelona <zandertabelona@gmail.com>
2023-10-02 08:39:45 +0800

    Learners Registration

--- resources/views/auth/learnersRegistration.blade.php
+++ resources/views/auth/learnersRegistration.blade.php
@@ -1,103 +1,170 @@
-{{-- @extends('layouts.app') --}}
+@extends('layouts.app')
 
+@section('title', 'Student Registration')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Student Registration') }}</div>
+<x-navbar />
+<div class="min-h-screen min-w-full flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 rounded-lg shadow-lg">
+        <h2 class="text-2xl font-semibold mb-6">{{ __('Student Registration') }}</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="/student/registered">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="name">{{ __('Name') }}</label>
-                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="middleName">{{ __('Middle Name') }}</label>
-                            <input id="middleName" type="text" class="form-control" name="middleName" value="{{ old('middleName') }}" autocomplete="middleName">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="lastName">{{ __('Last Name') }}</label>
-                            <input id="lastName" type="text" class="form-control" name="lastName" value="{{ old('lastName') }}" required autocomplete="lastName">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="sex">{{ __('Sex') }}</label>
-                            <select id="sex" class="form-control" name="sex" required>
-                                <option value="male" {{ old('sex') == 'male' ? 'selected' : '' }}>Male</option>
-                                <option value="female" {{ old('sex') == 'female' ? 'selected' : '' }}>Female</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="birthday">{{ __('Birthday') }}</label>
-                            <input id="birthday" type="date" class="form-control" name="birthday" value="{{ old('birthday') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="address">{{ __('Address') }}</label>
-                            <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">{{ __('Email') }}</label>
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="contact_number">{{ __('Contact Number') }}</label>
-                            <input id="contact_number" type="text" class="form-control" name="contact_number" value="{{ old('contact_number') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password">{{ __('Password') }}</label>
-                            <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm">{{ __('Confirm Password') }}</label>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="contactperson">{{ __('Contact Person') }}</label>
-                            <input id="contactperson" type="text" class="form-control" name="contactperson" value="{{ old('contactperson') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="contactperson_number">{{ __('Contact Person Number') }}</label>
-                            <input id="contactperson_number" type="text" class="form-control" name="contactperson_number" value="{{ old('contactperson_number') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="grade">{{ __('Grade') }}</label>
-                            <input id="grade" type="text" class="form-control" name="grade" value="{{ old('grade') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="section">{{ __('Section') }}</label>
-                            <input id="section" type="text" class="form-control" name="section" value="{{ old('section') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="lrn">{{ __('LRN (Learner\'s Reference Number)') }}</label>
-                            <input id="lrn" type="text" class="form-control" name="lrn" value="{{ old('lrn') }}" required>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Register') }}
-                            </button>
-                        </div>
-                    </form>
-                </div>
+        <form method="POST" action="/student/registered" class="space-y-4">
+            @csrf
+            <h2 class="font-bold text-blue-700 text-xl"> PERSONAL DETAILS </h2>
+            <hr>
+            <div class="flex flex-row space-x-4">
+                 <!-- Name -->
+            <div>
+                <label for="name" class="block text-gray-500">Name</label>
+                <input id="name" type="text" class="bg-gray-200 p-2 rounded-md" name="name"
+                    value="{{ old('name') }}" required autocomplete="name" autofocus>
+                @error('name')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
-        </div>
+
+            <!-- Middle Name -->
+            <div>
+                <label for="middleName" class="block text-gray-500">Middle Name</label>
+                <input id="middleName" type="text" class="bg-gray-200 p-2 rounded-md" name="middleName"
+                    value="{{ old('middleName') }}" autocomplete="middleName">
+            </div>
+
+            <!-- Last Name -->
+            <div>
+                <label for="lastName" class="block text-gray-500">Last Name</label>
+                <input id="lastName" type="text" class="bg-gray-200 p-2 rounded-md" name="lastName"
+                    value="{{ old('lastName') }}" required autocomplete="lastName">
+                @error('lastName')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            </div>
+
+            <div class="flex flex-row space-x-4">
+                 <!-- Sex -->
+            <div>
+                <label for="sex" class="block text-gray-500">Sex</label>
+                <select id="sex" class="bg-gray-200 p-2 rounded-md" style="width: 12.5rem;" name="sex" required>
+                    <option value="male" {{ old('sex') == 'male' ? 'selected' : '' }}>Male</option>
+                    <option value="female" {{ old('sex') == 'female' ? 'selected' : '' }}>Female</option>
+                </select>
+            </div>
+
+            <!-- Birthday -->
+            <div>
+                <label for="birthday" class="block text-gray-500">Birthday</label>
+                <input id="birthday" type="date" class="bg-gray-200 p-2 rounded-md" style="width: 12rem;" name="birthday"
+                    value="{{ old('birthday') }}" required>
+            </div>
+
+              <!-- Contact Number -->
+              <div>
+                <label for="contact_number" class="block text-gray-500">Contact Number</label>
+                <input id="contact_number" type="text" class="bg-gray-200 p-2 rounded-md" name="contact_number"
+                    value="{{ old('contact_number') }}" required>
+                @error('contact_number')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+
+
+
+            </div>
+
+            <!-- Address -->
+            <div>
+                <label for="address" class="block text-gray-500">Address</label>
+                <input id="address" type="text" class="bg-gray-200 p-2 rounded-md w-full" name="address"
+                    value="{{ old('address') }}" required>
+            </div>
+
+            <div class="flex flex-row space-x-4 w-full">
+
+                 <!-- Contact Person -->
+            <div class="w-1/2">
+                <label for="contactperson" class="block text-gray-500">Contact Person</label>
+                <input id="contactperson" type="text" class="bg-gray-200 p-2 rounded-md w-full" name="contactperson"
+                    value="{{ old('contactperson') }}" required>
+            </div>
+
+            <!-- Contact Person Number -->
+            <div class="w-1/2">
+                <label for="contactperson_number" class="block text-gray-500">Contact Person Number</label>
+                <input id="contactperson_number" type="text" class="bg-gray-200 p-2 rounded-md w-full" name="contactperson_number"
+                    value="{{ old('contactperson_number') }}" required>
+            </div>
+
+
+                
+            </div>
+
+            <h2 class="font-bold text-blue-700 text-xl"> STUDENT INFORMATION </h2>
+            <hr>
+
+            <div class="flex flex-row space-x-4 w-full">
+
+                
+            </div>
+
+
+            <!-- Email -->
+            <div>
+                <label for="email" class="block">Email</label>
+                <input id="email" type="email" class="form-input" name="email"
+                    value="{{ old('email') }}" required autocomplete="email">
+                @error('email')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+          
+
+            <!-- Password -->
+            <div>
+                <label for="password" class="block">Password</label>
+                <input id="password" type="password" class="form-input" name="password" required autocomplete="new-password">
+                @error('password')
+                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <!-- Confirm Password -->
+            <div>
+                <label for="password-confirm" class="block">Confirm Password</label>
+                <input id="password-confirm" type="password" class="form-input" name="password_confirmation" required
+                    autocomplete="new-password">
+            </div>
+
+           
+            <!-- Grade -->
+            <div>
+                <label for="grade" class="block">Grade</label>
+                <input id="grade" type="text" class="form-input" name="grade"
+                    value="{{ old('grade') }}" required>
+            </div>
+
+            <!-- Section -->
+            <div>
+                <label for="section" class="block">Section</label>
+                <input id="section" type="text" class="form-input" name="section"
+                    value="{{ old('section') }}" required>
+            </div>
+
+            <!-- LRN -->
+            <div>
+                <label for="lrn" class="block">LRN (Learner's Reference Number)</label>
+                <input id="lrn" type="text" class="form-input" name="lrn"
+                    value="{{ old('lrn') }}" required>
+            </div>
+
+            <div class="mt-6">
+                <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-full">
+                    {{ __('Register') }}
+                </button>
+            </div>
+        </form>
     </div>
 </div>
-{{-- @endsection --}}
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <link rel="icon" href="{{ asset('images/brightsparkslogo.png') }}" type="image/x-icon">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Tailwind -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+    <script src="https://kit.fontawesome.com/57a798c9bb.js" crossorigin="anonymous"></script>
+
+
+    <!-- Add your CSS and JavaScript links here -->
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+    
+    <x-footer />
+
+</body>
+
+</html>
